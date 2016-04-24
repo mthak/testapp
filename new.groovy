816cc596eaab0cb0d196995b7ce65a6a86006171
mthakkar <mthakkar@cloudera.com>
2016-04-24 12:41:49 -0700

    adding new groovy script to create jobs using jobdsl

--- new.groovy
+++ new.groovy
@@ -1,12 +1,5 @@
 import groovy.json.JsonSlurper
-def slurper = new JsonSlurper()
-
-def jobsJson = new JsonSlurper().parseText( new URL( 'https://api.github.com/repos/mthak/spark/git/trees/master?' ).text )
-//println jobsJson
-def giturl = "http://github.com/mthak/spark.git"
-def branch = "master"
-def command = "-e clean deploy -DskipTests"
-categorizedJobsView('APM-Jobs') {
+bsView('APM-Jobs') {
     jobs {
     }
     categorizationCriteria {
@@ -18,7 +11,13 @@ categorizedJobsView('APM-Jobs') {
         buildButton()
     }
 }
+def slurper = new JsonSlurper()
 
+def jobsJson = new JsonSlurper().parseText( new URL( 'https://api.github.com/repos/mthak/spark/git/trees/master?' ).text )
+//println jobsJson
+def giturl = "http://github.com/mthak/spark.git"
+def branch = "master"
+def command = "-e clean deploy -DskipTests"
 jobsJson.tree.each { 
 if (it.type == 'tree' && it.path != '.github') {
    path = it.path
@@ -39,3 +38,16 @@ mavenJob("APM-${it.path}") {
 }
 }
 
+categorizedJobsView('APM-Jobs') {
+    jobs {
+        regex(/APM-.*/)
+    }
+    categorizationCriteria {
+        regexGroupingRule(/^APM-.*$/)
+    }
+    columns {
+        status()
+        categorizedJob()
+        buildButton()
+    }
+}
