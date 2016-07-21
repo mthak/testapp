03783eb0e02422164690a288022658ac976a2c4f
mthakkar <mthakkar@cloudera.com>
2016-07-20 22:15:23 -0700

    make changes to support packer ami

--- my-cloud-deploy/scripts/find_process.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Find if a porcess is already using the port required by this application 
-# iF yes then kill that process 
-port=$1
-lsof -i :$port | tail -n +2 | awk '{print $2}'| xargs kill -9
